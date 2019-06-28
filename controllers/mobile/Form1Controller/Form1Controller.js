b83b626b32d5603396ee2257a5dc81bb935c31ee
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2019-06-28 02:32:41 +0200

    Fix layout problem with message and login button
    Clean up and internationalize registration button.

--- controllers/mobile/Form1Controller/Form1Controller.js
+++ controllers/mobile/Form1Controller/Form1Controller.js
@@ -11,32 +11,47 @@ define(function(){
 		/* Replace this made-up logic with your own.
 		Ideally you want to call a service with these credentials.*/
 		if(user === dummy.user && password === dummy.password){
-			view.Login.setMessage("Success!");
+			view.Login.showMessage("Success!");
 		}
 		else{
-			view.Login.setMessage(`Login failed.`);
+			view.Login.showMessage(`Login failed.`);
 			alert(`Try user ${dummy.user} and password ${dummy.password}`);
 		}
 	}
 
+	function onValidInputs(){
+		//TODO: Logic on syntactically valid inputs.
+		kony.print("TODO: Logic on syntactically valid inputs.");
+	}
+
 	function onInvalidInputs(){
-		//TODO: Call service to show captcha, lock account, set the dogs loose, etc.
+		//TODO: Logic on syntactically invalid inputs, like captchas, block account, etc.
+		kony.print("TODO: Logic on syntactically invalid inputs, like captchas, block account, etc.");
 	}
 
 	return {
 
 		preShow: function(){
-			view = this.view;	
+			view = this.view;
+
+			//amplify.publish("Form1.preShow");
+			this.view.Login.preShow();
 		},
 
 		postShow: function(){
+			this.view.Login.onLogin = login;
 			/*global amplify*/
-			amplify.subscribe("Login.valid", login);
+			amplify.subscribe("Login.onLogin", login);
+			amplify.subscribe("Login.valid", onValidInputs);
 			amplify.subscribe("Login.invalidUser", onInvalidInputs);
 			amplify.subscribe("Login.invalidPassword", onInvalidInputs);
+
+			//amplify.publish("Form1.postShow");
+			this.view.Login.postShow();
 		},
 
 		onNavigate: function(){
+			//kony.mvc.patch(this, true);
 			kony.mvc.patch(this);
 		}
 	};
--- userwidgets/com.mig82.Login/modules/require/LoginController.js
+++ userwidgets/com.mig82.Login/modules/require/LoginController.js
@@ -2,7 +2,12 @@ define(function() {
 
 	//TODO: Expose fields or function to allow the developer to set the password regex.
 	const userRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	const passwordRegex = /^([a-zA-Z0-9@!#+%&$]{2,})$/;
+	const passwordRegex = /^([a-zA-Z0-9@!#+%&$]{3,})$/;
+	const minUserLength = 5;
+	const minPasswordLength = 3;
+
+	var messageLabelWidth;
+	var loginButtonWidth;
 
 	return {
 		togglePasswordVisibility: function _togglePasswordVisibility(/*widget, x, y*/){
@@ -20,11 +25,29 @@ define(function() {
 			}
 		},
 
-		toggleMessageVisibility: function _toggleMessageVisibility(makeVisible){
+		toggleMessageLabel: function _toggleMessageLabel(makeVisible){
 			//TODO: Do this with an animation to fade in or out.
-			this.view.messageLabel.setVisibility(makeVisible);
+			if(makeVisible){
+				this.view.messageLabel.width = messageLabelWidth;
+			}
+			else{
+				this.view.messageLabel.width = "0%";
+			}
+			this.view.forceLayout();
 		},
 
+		toggleLoginButton: function _toggleLoginButton(makeVisible){
+			//TODO: Do this with an animation to fade in or out.
+			if(makeVisible){
+				this.view.loginButton.width = loginButtonWidth;
+			}
+			else{
+				this.view.loginButton.width = "0%";
+			}
+			this.view.forceLayout();
+		},
+
+		//We also expose this function for the parent form to set a message resulted from the authentication attempt.
 		showMessage: function _showMessage(message){
 			if(typeof kony.i18n.getLocalizedString2 === "function"){
 				this.view.messageLabel.text = kony.i18n.getLocalizedString2(message);
@@ -32,49 +55,67 @@ define(function() {
 			else{
 				this.view.messageLabel.text = kony.i18n.getLocalizedString(message);
 			}
-			this.toggleMessageVisibility(true);
+			this.toggleMessageLabel(true);
 		},
 
-		login: function _login(){
-			this.toggleMessageVisibility(false);
+		validateInputs: function _validateInputs(){
+			this.toggleMessageLabel(false);
 			let user = this.view.userTextBox.text;
 			let password = this.view.passwordTextBox.text;
 
 			//If both the user and password fields have more than 4 characters each.
-			if(user && !userRegex.test(user)){
+			if(!userRegex.test(user)){
 				//TODO: Expose field for invalid password i18n.
-				this.showMessage(this._invalidUserMessage);
-				amplify.publish("Login.invalidUser");
+				if(user.length >= minUserLength){
+					this.showMessage(this._invalidUserMessage);
+					amplify.publish("Login.invalidUser");
+				}
+				this.toggleLoginButton(false);
 			}
-			else if(password && !passwordRegex.test(password)){
+			else if(!passwordRegex.test(password)){
 				//TODO: Expose field for invalid password i18n.
-				this.showMessage(this._invalidPasswordMessage);
-				amplify.publish("Login.invalidPassword");
+				if(password.length >= minPasswordLength){
+					this.showMessage(this._invalidPasswordMessage);
+					amplify.publish("Login.invalidPassword");
+				}
+				this.toggleLoginButton(false);
 			}
-			else if(user && password && userRegex.test(user) & passwordRegex.test(password)){
+			else if(userRegex.test(user) & passwordRegex.test(password)){
 				/*global amplify*/
 				amplify.publish("Login.valid", user, password);
+				this.toggleLoginButton(true);
 			}
 		},
 
-		setMessage: function(message){
-			this.view.messageLabel.text = message;
-			this.view.messageLabel.isVisible = true;
+		preShow: function(){
+			//Hide the message and the login button. They're visible for design purposes only.
+			messageLabelWidth = this.view.messageLabel.width;
+			this.toggleMessageLabel(false);
+			loginButtonWidth = this.view.loginButton.width;
+			this.toggleLoginButton(false);
 		},
 
-		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
-
-			//Hide the message label. It's visible for design purposes only.
-			this.view.messageLabel.isVisible = false;
-
+		postShow: function(){
 			//Bind the eye icon to show or hide the password.
 			this.view.showPasswordButton.onTouchEnd = this.togglePasswordVisibility;
 
 			//Bind user and password fields to attempt login once both are filled.
-			this.view.userTextBox.onDone = this.login;
-			this.view.passwordTextBox.onDone = this.login;
-			this.view.onKeyboardDidHide = this.login;
+			this.view.userTextBox.onTextChange = this.validateInputs;
+			this.view.passwordTextBox.onTextChange = this.validateInputs;
+			this.view.onKeyboardDidHide = this.validateInputs;
+
+			this.view.loginButton.onTouchEnd = () => {
+				//kony.print("Clicked to sign in");
+				amplify.publish(
+					"Login.onLogin",
+					this.view.userTextBox.text,
+					this.view.passwordTextBox.text
+				);
+			};
 		},
+
+		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {},
+
 		//Logic for getters/setters of custom properties
 		initGettersSetters: function() {
 			defineGetter(this, "invalidUserMessage", function () {
