ca3d28d299e2300e9d046008ca616a2541972bbf
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2019-06-28 02:26:50 +0200

    Add logic to autom. bind all components in a form
    This can be useful in very large forms to make them easier to bind.

---
+++ modules/extensions/ui_getDescendants.js
@@ -0,0 +1,44 @@
+
+/**
+* getDescendants - Returns an array containing all the widgets nested within a form or container widget.
+* The container widget may be a Form, a Flex Container, Scroll Flex Container or any other widget capable
+* of containing other widgets.
+*
+* @param  {FlexForm|FlexContainer|FlexFormContainer|Segment} containerWidget The parent form or container widget
+* for which you wish to get all descendants.
+* @param  {Boolean} includeParent   Whether to include the parent container widget in the
+* resulting array.
+* @param  {Function} test   A function used to determine which children must be included in the result.
+* @return {Array} An array containing all widgets within a form or container widget.
+*
+* @author Miguelángel Fernández
+*/
+((definition) => {
+	kony.ui.getDescendants = definition;
+})(function getDescendants(containerWidget, includeParent, test){
+
+	//A function that given a widget, returns its children matching a test
+	function getChildren(parent, t){
+		var filtered = typeof t === "function";
+		var descendants = [];
+		if(typeof parent.widgets === "function"){
+			let children = parent.widgets();
+			if(filtered) children = children.filter(t);
+			descendants = descendants.concat(children);
+		}
+		return descendants;
+	}
+
+	//If a widget is considered a descendant of itself, then start by putting it in the array.
+	var descendants = includeParent?[containerWidget]:[];
+	if(typeof test === "function") descendants = descendants.filter(test);
+
+	//Then add the children of the parent.
+	descendants = descendants.concat(getChildren(containerWidget, test));
+
+	//Then add the children of each child already known.
+	for(var k = 0; k < descendants.length; k++){
+		descendants = descendants.concat(getChildren(descendants[k], test));
+	}
+	return descendants;
+});
